bc4c0f40bca69633da084985b175cad82189146f
dominick038 <dominickherweijer@gmail.com>
2025-12-03 10:58:19 +0100

    part 2 answered wooooooooo

--- day_03/main.kt
+++ day_03/main.kt
@@ -1,15 +1,21 @@
 package day03
 
 import shared.readInput;
+import kotlin.text.toInt
 
 fun main() {
     val input = readInput("day03");
 
     var result: Int = 0;
+    var result2: Long = 0;
     for (line in input) {
+        var i = 0;
+
         var largestNum = 0;
         var secondLargestNum = 0;
-        var i = 0;
+        
+        val stack: ArrayDeque<Int> = ArrayDeque(128);
+        var toRemove = line.length - 12;
         line.forEach { char -> 
             val num = char.digitToInt();
             val isLastIndex = i == 99;
@@ -23,13 +29,25 @@ fun main() {
 
             if (!hasChanged && num > secondLargestNum) {
                 secondLargestNum = num;
-            } 
+            }
             
+            while (stack.isNotEmpty() &&
+                   stack.first() < num &&
+                   toRemove > 0) {
+            
+                stack.removeFirst();
+                toRemove -= 1;
+            }
+
+            stack.addFirst(num)
+
             i += 1; 
         }
 
         result += (largestNum.toString() + secondLargestNum.toString()).toInt();
+        result2 += stack.reversed().take(12).joinToString("").toLong();
     }
 
-    println("Total joltage across banks???: $result");
+    println("Total joltage across 2  batteries: $result");
+    println("Total joltage across 12 batteries: $result2");
 }
