7a13c341e2e5d44c00607dbd3520338e4ca3d4ce
dominick038 <dominickherweijer@gmail.com>
2025-12-03 08:43:14 +0100

    day 3 part one added to the solutions

--- day_03/main.kt
+++ day_03/main.kt
@@ -1,5 +1,35 @@
 package day03
 
+import shared.readInput;
+
 fun main() {
-    println("Hello, World!")
+    val input = readInput("day03");
+
+    var result: Int = 0;
+    for (line in input) {
+        var largestNum = 0;
+        var secondLargestNum = 0;
+        var i = 0;
+        line.forEach { char -> 
+            val num = char.digitToInt();
+            val isLastIndex = i == 99;
+
+            var hasChanged = false;
+            if (num > largestNum && !isLastIndex) {
+                largestNum = num;
+                secondLargestNum = 0;
+                hasChanged = true;
+            }
+
+            if (!hasChanged && num > secondLargestNum) {
+                secondLargestNum = num;
+            } 
+            
+            i += 1; 
+        }
+
+        result += (largestNum.toString() + secondLargestNum.toString()).toInt();
+    }
+
+    println("Total joltage across banks???: $result");
 }
