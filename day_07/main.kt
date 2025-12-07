296c28e48bd23a4c33293a651f82b17be53a64d8
dominick038 <dominickherweijer@gmail.com>
2025-12-07 10:45:09 +0100

    Removed printlns

--- day_07/main.kt
+++ day_07/main.kt
@@ -18,20 +18,15 @@ fun main() {
     stack.addFirst(Vector2D(positionOfS, 0));
     
     while (stack.isNotEmpty()) {
-        println("entered stack check")
         var currentBeam = stack.removeFirst();
         while (true) {
-            println("creating new position");
             currentBeam = currentBeam.copy(Y = currentBeam.Y + 1, X = currentBeam.X);
             if (currentBeam.Y > inputLength || visitedPositions.contains(currentBeam)) break;
             
-            println("checking if position is period");
             if (input[currentBeam.Y][currentBeam.X] == '.') {
-                println("adding to visited positions");
                 visitedPositions.add(currentBeam);
             } 
             else {
-                println("trtinnfgnf to add splitter");
                 val leftOfSplitter  = currentBeam.copy(Y = currentBeam.Y, X = currentBeam.X - 1);
                 val rightOfSplitter = currentBeam.copy(Y = currentBeam.Y, X = currentBeam.X + 1);
                 if (leftOfSplitter.X >= 0 && !visitedPositions.contains(leftOfSplitter)) {
