1b085784353e940712f5d2c28ebc62bf051db58a
dominick038 <dominickherweijer@gmail.com>
2025-12-07 13:01:21 +0100

    day 2 part two added

--- day_07/main.kt
+++ day_07/main.kt
@@ -3,6 +3,7 @@ package day07
 import shared.readInput;
 
 data class Vector2D(var X: Int, var Y: Int) {}
+data class BeamState(val X: Int, val Y: Int, val dX: Int, val dY: Int)
 
 fun main() {
     var totalSplitterHits: Int = 0;
@@ -10,41 +11,43 @@ fun main() {
     val input = readInput("day07");
     val inputLength = input.size - 1;
     val rowLength   = input[0].length;
-    
+
     val positionOfS = input[0].indexOf('S');
-    
+
     val visitedPositions: HashSet<Vector2D> = HashSet();
-    val stack: ArrayDeque<Vector2D> = ArrayDeque(128);
-    stack.addFirst(Vector2D(positionOfS, 0));
-    
-    while (stack.isNotEmpty()) {
-        var currentBeam = stack.removeFirst();
-        while (true) {
-            currentBeam = currentBeam.copy(Y = currentBeam.Y + 1, X = currentBeam.X);
-            if (currentBeam.Y > inputLength || visitedPositions.contains(currentBeam)) break;
-            
-            if (input[currentBeam.Y][currentBeam.X] == '.') {
-                visitedPositions.add(currentBeam);
-            } 
-            else {
-                val leftOfSplitter  = currentBeam.copy(Y = currentBeam.Y, X = currentBeam.X - 1);
-                val rightOfSplitter = currentBeam.copy(Y = currentBeam.Y, X = currentBeam.X + 1);
-                if (leftOfSplitter.X >= 0 && !visitedPositions.contains(leftOfSplitter)) {
-                    stack.addFirst(leftOfSplitter);
-                }
+    val pathCache: HashMap<BeamState, Long> = HashMap();
 
-                if (rightOfSplitter.X < rowLength && !visitedPositions.contains(rightOfSplitter)) {
-                    stack.addFirst(rightOfSplitter);
-                }
-                
-                totalSplitterHits++;
+    fun countPaths(state: BeamState): Long {
+        pathCache[state]?.let { return it }
 
-                break;
-            }
+        val newX = state.X + state.dX;
+        val newY = state.Y + state.dY;
+
+        if (newY > inputLength || newY < 0 || newX < 0 || newX >= rowLength) return 1;
+
+        val cell = input[newY][newX];
+
+        if (cell == '^') totalSplitterHits++;
+
+        val newPos = Vector2D(newX, newY);
+        if (!visitedPositions.contains(newPos)) visitedPositions.add(newPos);
+
+        val result = if (cell == '.') {
+            countPaths(BeamState(newX, newY, state.dX, state.dY))
+        } else {
+            var total = 0L;
+            if (newX - 1 >= 0) total += countPaths(BeamState(newX - 1, newY, 0, 1));
+            if (newX + 1 < rowLength) total += countPaths(BeamState(newX + 1, newY, 0, 1));
+            total
         }
+
+        pathCache[state] = result;
+        return result;
     }
 
+    val totalTimelines = countPaths(BeamState(positionOfS, 0, 0, 1));
 
     println("Total splitter hits: $totalSplitterHits");
+    println("Total quantum timelines: $totalTimelines");
 }
 
