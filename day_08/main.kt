cb671a71e8974862f968813dd80fec3d0ed7d2eb
dominick038 <dominickherweijer@gmail.com>
2025-12-08 15:52:23 +0100

    Implemented result with union find this took only 3.5 hours 💀💀💀

--- day_08/main.kt
+++ day_08/main.kt
@@ -26,9 +26,49 @@ data class distanceRecord(val distance: Long, val vectorAId: Int, val vectorBId:
     }
 };
 
+class UnionFind(private val totalSize: Int) {
+    private val parent = IntArray(totalSize) { it }
+    private val size = IntArray(totalSize) { 1 }
+
+    fun find(x: Int): Int {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+
+    fun union(x: Int, y: Int): Boolean {
+        val rootX = find(x)
+        val rootY = find(y)
+
+        if (rootX == rootY) return false
+
+        parent[rootX] = rootY
+        size[rootY] += size[rootX]
+        return true
+    }
+
+    fun getUnionSizes(): Map<Int, Int> {
+        val sizes = mutableMapOf<Int, Int>()
+        for (i in parent.indices) {
+            val root = find(i)
+            sizes[root] = size[root]
+        }
+        return sizes
+    }
+
+    fun isFullyConnected(): Boolean {
+        val root = find(0)
+        return size[root] == totalSize
+    }
+}
+
 fun main() {
-    val input = readInput("day08");    
+    val input = readInput("day08");
     val inputSize = input.size;
+
+    var largestUnionResult = 1;
+    var lastConnectionProductResult: Long = 0;
 
     val listOfDistances = mutableListOf<distanceRecord>();
     val vectorArray = Array<Vector3D>(inputSize) { Vector3D(0, 0, 0) };
@@ -67,10 +107,35 @@ fun main() {
     }
 
     listOfDistances.sortBy { it.distance };
-   
-    for (distance in listOfDistances) {
+
+    val unionFind = UnionFind(inputSize);
+    val connectionsToMake = if (inputSize == 20) 10 else 1000;
+
+    for (i in 0 until minOf(connectionsToMake, listOfDistances.size)) {
+        val distance = listOfDistances[i];
+        unionFind.union(distance.vectorAId, distance.vectorBId);
     }
-    println("total items in list ${listOfDistances.size}");
+
+    val unionSizes = unionFind.getUnionSizes();
+    val top3 = unionSizes.values.sortedDescending().take(3);
+    for (size in top3) {
+        largestUnionResult = largestUnionResult * size;
+    }
+
+    val unionFind2 = UnionFind(inputSize);
+    for (distance in listOfDistances) {
+        val wasUnified = unionFind2.union(distance.vectorAId, distance.vectorBId);
+
+        if (wasUnified && unionFind2.isFullyConnected()) {
+            val vectorA = vectorArray[distance.vectorAId];
+            val vectorB = vectorArray[distance.vectorBId];
+            lastConnectionProductResult = vectorA.X.toLong() * vectorB.X.toLong();
+            break;
+        }
+    }
+
+    println("Largest circuit union: $largestUnionResult");
+    println("Last connection product: $lastConnectionProductResult");
 }
 
 
